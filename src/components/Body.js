726c3c3bf4ca171b3716abb0f067119ffd90c954
shivamxaryan <shivamaryan64@gmail.com>
2023-09-22 01:58:08 +0530

    using useState react hook on resList's data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,62 +1,10 @@
 import ResCard from "./ResCard";
-// import resList from "../utils/resList";
+import resList from "../utils/resList";
 import { useState } from "react";
 
 const Body = () => {
 
-  const [restaurantList, setrestaurantList] = useState([
-    {
-      data:{
-        id: "74453",
-        name: "Domino's Pizza",
-        uuid: "87727dbd-7f2b-4857-9763-359624165845",
-        city: "21",
-        area: "Athwa",
-        totalRatingsString: "1000+ ratings",
-        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-        cuisines: ["Pizzas"],
-        costForTwo: 40000,
-        address:
-          "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
-        locality: "Bhatar Road",
-        avgRating: "4.3",
-      }
-    },
-    {
-      data:{
-        id: "74454",
-        name: "Pizza Hut",
-        uuid: "87727dbd-7f2b-4857-9763-359624165845",
-        city: "21",
-        area: "Athwa",
-        totalRatingsString: "1000+ ratings",
-        cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-        cuisines: ["Pizzas"],
-        costForTwo: 40000,
-        address:
-          "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
-        locality: "Bhatar Road",
-        avgRating: "4.0",
-      }
-  },
-  {
-    data:{
-      id: "74455",
-      name: "KFC",
-      uuid: "87727dbd-7f2b-4857-9763-359624165845",
-      city: "21",
-      area: "Athwa",
-      totalRatingsString: "1000+ ratings",
-      cloudinaryImageId: "bz9zkh2aqywjhpankb07",
-      cuisines: ["Pizzas"],
-      costForTwo: 40000,
-      address:
-        "Shop 32 To 35, Sheetal Shopping Square,Near Lb Turning Point, Bhatar Road,MAJURA NONDH, Surat,GUJARAT-395001",
-      locality: "Bhatar Road",
-      avgRating: "4.1",
-    }
-},
-  ]);
+  const [restaurantList, setrestaurantList] = useState(resList);
 
 return(
     <div className="body-container">
