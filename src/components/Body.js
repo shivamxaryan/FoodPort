487e68a62bbb77b2aa0f2ab084759a9fa8b4e06f
shivamxaryan <shivamaryan64@gmail.com>
2023-09-29 23:08:08 +0530

    using useEffect and getting live data from swiggy

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,38 +1,60 @@
 import ResCard from "./ResCard";
-import resList from "../utils/resList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
+  const [restaurantList, setrestaurantList] = useState([]);
 
-  const [restaurantList, setrestaurantList] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-return(
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6263764&lng=77.2090704&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+
+    //optional chaining
+    setrestaurantList(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  //conditional rendering
+  // if(restaurantList.length == 0){
+  //   return <Shimmer/>;
+  // }
+  return restaurantList.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body-container">
   
       {/* <div className="search">Search</div>  */}
 
       <div className="filter-card">
-
-        <button className="filter-btn" 
-        onClick={() => {
-          const resupdatedlist=restaurantList.filter(
-            (res) => res.data.avgRating > 4
-          );
-          setrestaurantList(resupdatedlist);
-        }}>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const resupdatedlist = restaurantList.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setrestaurantList(resupdatedlist);
+          }}
+        >
           Top Rated Restaurant
         </button>
 
       </div>
 
       <div className="res-list">
-      {restaurantList.map((restaurant) => {
-          return <ResCard key={restaurant.data.id} {...restaurant.data} />;
+        {restaurantList?.map((restaurant) => {
+          return <ResCard key={restaurant?.info?.id} {...restaurant?.info} />;
         })}
       </div>
   
     </div>
   );
-  }
+};
 
-  export default Body;
+export default Body;
