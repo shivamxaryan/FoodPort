53152697d83f76d4ce2556c0d013b470b0989dcd
shivamxaryan <shivamaryan64@gmail.com>
2023-09-29 23:27:25 +0530

    adding login logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const[logbtn, setlogbtn]=useState("Login");
+
     return (
       <div className="header">
         <div className="logo-container">
           <img className="logo" src={LOGO_URL} alt="logo" />
         </div>
-  
+
         <div className="nav-items">
           <ul>
             <li>Home</li>
             <li>About Us</li>
             <li>Contact Us</li>
             <li>Cart</li>
+            <li
+              className="login-btn"
+              onClick={()=>{
+                logbtn === "Login"
+                  ? setlogbtn("LogOut")
+                  : setlogbtn("Login")
+              }}
+            >
+              {logbtn}
+            </li>
           </ul>
         </div>
       </div>
