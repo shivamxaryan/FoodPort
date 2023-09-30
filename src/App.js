3612be224b7219af26eef236b1bd933cfe0dc6b3
shivamxaryan <shivamaryan64@gmail.com>
2023-09-30 19:10:34 +0530

    adding routes in App and using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
 
 const AppLayout = () => (
   <div className="app-layout">
@@ -12,5 +13,17 @@ const AppLayout = () => (
   </div>
 );
 
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<Error />
+  },
+  {
+    path:"/about",
+    element:<About />
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter} />);
