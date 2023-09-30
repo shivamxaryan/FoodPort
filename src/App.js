2f367b9752858570611da0864b99f5f6a707a8e9
shivamxaryan <shivamaryan64@gmail.com>
2023-09-30 19:24:41 +0530

    using Outlet for rendering children page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
 
 const AppLayout = () => (
   <div className="app-layout">
     <Header />
-    <Body />
+    <Outlet />
   </div>
 );
 
@@ -17,11 +17,17 @@ const appRouter = createBrowserRouter([
   {
     path:"/",
     element:<AppLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<Body />
+      },
+      {
+        path:"/about",
+        element:<About />
+      }
+    ],
     errorElement:<Error />
-  },
-  {
-    path:"/about",
-    element:<About />
   }
 ])
 
