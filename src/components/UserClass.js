0f559291a3eb9aca0a53d709cf7941d11d3b03df
shivamxaryan <shivamaryan64@gmail.com>
2023-10-10 15:29:06 +0530

    using reacthooks in class component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,30 +1,34 @@
 import React from "react";
 import { useState } from "react";
 
-class UserClass extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            count1:0,
-        }
-    }
-    render(){
-        const {name}= this.props;
-        const {count1}= this.state;
-    return (
-        <div className="user-card">
-            <h2>Count:{count1}</h2>
-            <button onClick={()=>{
-               this.setState({
-                count1:this.state.count1+1
-               });
-            }}>countInc</button>
-            <h3>Name: {name}</h3>
-            <h3>Role: MERN Stack Developer</h3>
-            <h3>Location: Bangluru</h3>
-        </div>
-    );
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count1: 0,
     };
+  }
+  render() {
+    const { name } = this.props;
+    const { count1 } = this.state;
+    return (
+      <div className="user-card">
+        <h2>Count:{count1}</h2>
+        <button
+          onClick={() => {
+            this.setState({
+              count1: this.state.count1 + 1,
+            });
+          }}
+        >
+          countInc
+        </button>
+        <h3>Name: {name}</h3>
+        <h3>Role: MERN Stack Developer</h3>
+        <h3>Location: Bangluru</h3>
+      </div>
+    );
+  }
 }
 
 export default UserClass;
