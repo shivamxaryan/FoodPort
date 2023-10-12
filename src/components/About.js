2b04c176087574177757f1eb42049293cb82cddf
shivamxaryan <shivamaryan64@gmail.com>
2023-10-12 15:56:45 +0530

    using componentDidMount, state and setState

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,6 @@ const About = () => {
     return(
         <div>
             <h1>About Us Page</h1>
-            {/* <User name={"Shivam Function"}/> */}
             <UserClass name={"Shivam Class"}/>
         </div>
     )
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,28 +4,36 @@ import { useState } from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      count1: 0,
+      userInfo: {
+        name: "Dummy Name",
+        location: "Default Location",
+      },
     };
   }
+
+  // how to call api in classs component
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/shivamxaryan");
+    const json = await data.json();
+
+    console.log(json);
+
+    this.setState({
+        userInfo:json,
+    });
+  }
+
   render() {
-    const { name } = this.props;
-    const { count1 } = this.state;
+    const { name, location, avatar_url } = this.state.userInfo;
+
     return (
       <div className="user-card">
-        <h2>Count:{count1}</h2>
-        <button
-          onClick={() => {
-            this.setState({
-              count1: this.state.count1 + 1,
-            });
-          }}
-        >
-          countInc
-        </button>
+        <img src={"avatar_url"} />
         <h3>Name: {name}</h3>
         <h3>Role: MERN Stack Developer</h3>
-        <h3>Location: Bangluru</h3>
+        <h3>Location: {location}</h3>
       </div>
     );
   }
