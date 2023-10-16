b6704aec946e20e307ac3cc584beb901c1131d7d
shivamxaryan <shivamaryan64@gmail.com>
2023-10-16 16:12:56 +0530

    adding online status using custom hook in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
   const[logbtn, setlogbtn]=useState("Login");
+  const onlineStatus = useOnlineStatus();
 
     return (
       <div className="header">
@@ -13,6 +15,7 @@ const Header = () => {
 
         <div className="nav-items">
           <ul>
+            <li>Online Status:{onlineStatus?"🟢":"🔴"}</li>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About Us</Link></li>
             <li><Link to="/contact">Contact Us</Link></li>
