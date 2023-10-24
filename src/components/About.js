d55036f0037c10cb993e65ac6937be08d64fa148
shivamxaryan <shivamaryan64@gmail.com>
2023-10-24 17:04:26 +0530

    updating shimmer

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,42 @@
-import User from "./User";
-import UserClass from "./UserClass";
+// import { useContext } from "react";
+// import { Outlet } from "react-router-dom";
+// // import ProfileFunction from "./ProfileFunction";
+// import UserClass from "./UserClass";
 
+// const AboutPage = () => {
 
-const About = () => {
-    return(
-        <div>
-            <h1>About Us Page</h1>
-            <UserClass name={"Shivam Class"}/>
-        </div>
-    )
-};
+//   return (
+//     <div>
+//       <Outlet />
+//       {/* <ProfileClass name={"Harshit Pachori"} location={"Aligarh"} /> */}
+//       <UserClass />
+//       <h1>About us page</h1>
+//       <input
+//         value={user.name}
+//         onChange={(e) =>
+//           setUser({
+//             ...user,
+//             name: e.target.value,
+//           })
+//         }
+//         className="border ring-1  ring-slate-300 px-2"
+//       />
+//       <input
+//         value={user.email}
+//         onChange={(e) =>
+//           setUser({
+//             ...user,
+//             email: e.target.value,
+//           })
+//         }
+//         className="border ring-1  ring-slate-300 px-2"
+//       />
+//       <span className="text-black font-semibold p-5">
+//         {user.name} - {user.email}
+//       </span>
+//       <h3>This is about us page of our swiggy clone foodVilla application</h3>
+//     </div>
+//   );
+// };
 
-export default About;
+// export default AboutPage;
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,26 +1,22 @@
+import React from "react";
+
+const ShimmerCard = () => {
+  return (
+    <div className=" m-2 w-[300px] rounded-lg h-[230px] bg-slate-200 shadow-md">
+      <div className="bg-slate-100 h-[130px] rounded-lg"></div>
+      <h3 className="bg-slate-100 h-[20px] m-5 rounded-lg"></h3>
+      <div className="bg-slate-100 h-[20px] m-5 rounded-lg"></div>
+    </div>
+  );
+};
 const Shimmer = () => {
-    return (
-        <div className="shimmer-container">
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-            <div className="shimmer-card"></div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-wrap gap-5 mt-16 items-center justify-center">
+      {new Array(10).fill(0).map((element, index) => (
+        <ShimmerCard key={`index-` + index} />
+      ))}
+    </div>
+  );
+};
 
 export default Shimmer;
