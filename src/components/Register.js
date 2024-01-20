d4bf686d2bcf0eb2502b76ad986a5f260d0973be
Shivam <105743328+shivamxaryan@users.noreply.github.com>
2024-01-20 14:24:42 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,62 +1,69 @@
-const Register=() => {
-    return (
-        <div>
-              <h2 class="text-3xl pt-12 font-bold text-center text-black-900">
-                Sign Up
-            </h2>
-            <div className="relative flex flex-col justify-center p-6 m-9 overflow-hidden">
-            <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
-                <form className="mt-6">
-                <div className="mb-2">
-                        <label
-                            for="text"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            for="email"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    <div className="mb-2">
-                        <label
-                            for="password"
-                            className="block text-sm font-semibold text-gray-800"
-                        >
-                            Password
-                        </label>
-                        <input
-                            type="password"
-                            className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
-                        />
-                    </div>
-                    {/* <a
+const refreshPage = () => {
+  window.location.reload(false);
+};
+
+const Register = () => {
+  return (
+    <div>
+      <h2 class="text-3xl pt-12 font-bold text-center text-black-900">
+        Sign Up
+      </h2>
+      <div className="relative flex flex-col justify-center p-6 m-9 overflow-hidden">
+        <div className="w-full p-6 m-auto bg-white rounded-md shadow-md lg:max-w-xl">
+          <form className="mt-6">
+            <div className="mb-2">
+              <label
+                for="text"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                for="email"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            <div className="mb-2">
+              <label
+                for="password"
+                className="block text-sm font-semibold text-gray-800"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                className="block w-full px-4 py-2 mt-2 text-slate-700 bg-white border rounded-md focus:border-slate-400 focus:ring-slate-300 focus:outline-none focus:ring focus:ring-opacity-40"
+              />
+            </div>
+            {/* <a
                         href="#"
                         className="text-xs text-slate-600 hover:underline"
                     >
                         Forget Password?
                     </a> */}
-                    <div className="mt-6">
-                        <div className="w-full cursor-pointer text-center px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-slate-700 rounded-md hover:bg-slate-600 focus:outline-none focus:bg-slate-600">
-                            Sign up
-                        </div>
-                    </div>
-                </form>
+            <div className="mt-6">
+              <div
+                className="w-full cursor-pointer text-center px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-slate-700 rounded-md hover:bg-slate-600 focus:outline-none focus:bg-slate-600"
+                onClick={refreshPage}
+              >
+                Sign up
+              </div>
+            </div>
+          </form>
 
-                {/* <p className="mt-8 text-xs font-light text-center text-gray-700">
+          {/* <p className="mt-8 text-xs font-light text-center text-gray-700">
                     {" "}
                     Don't have an account?{" "}
                     <a
@@ -66,10 +73,10 @@ const Register=() => {
                        <span>Sign up</span>
                     </a>
                 </p> */}
-            </div>
         </div>
-        </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Register;
